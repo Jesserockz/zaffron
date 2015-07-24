60468f2621550ad445a8a2d681bb108eb47f0315
Jesse Hills <jesserockz@gmail.com>
2015-07-24 15:05:19 +1200

    Add calculator script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /Users/touchtech/workspace/zaffron
 git pull origin master
+npm install
 launchctl unload ~/Library/LaunchAgents/nz.co.touchtech.zaffron.plist
 launchctl load ~/Library/LaunchAgents/nz.co.touchtech.zaffron.plist
